4fb6ff5166e411d1f9c45c8ddde80f81b614368e
dangyingkai <dangyingkai@bytedance.com>
2026-01-09 20:34:44 +0800

    add opencode.nvim plugin with custom keybindings

---
+++ nvim/lua/plugins/opencode.lua
@@ -0,0 +1,33 @@
+return {
+  "NickvanDyke/opencode.nvim",
+  dependencies = {
+    { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+  },
+  config = function()
+    vim.o.autoread = true
+
+    vim.keymap.set({ "n", "x" }, "<leader>a", function()
+      require("opencode").ask("@this: ", { submit = true })
+    end, { desc = "Ask opencode" })
+    vim.keymap.set({ "n", "x" }, "<leader><C-a>", function()
+      require("opencode").select()
+    end, { desc = "Execute opencode action…" })
+    vim.keymap.set({ "n", "t" }, "<c-.>", function()
+      require("opencode").toggle()
+    end, { desc = "Toggle opencode" })
+
+    vim.keymap.set({ "n", "x" }, "go", function()
+      return require("opencode").operator("@this ")
+    end, { expr = true, desc = "Add range to opencode" })
+    vim.keymap.set("n", "goo", function()
+      return require("opencode").operator("@this ") .. "_"
+    end, { expr = true, desc = "Add line to opencode" })
+
+    vim.keymap.set("n", "<s-c-u>", function()
+      require("opencode").command("session.half.page.up")
+    end, { desc = "opencode half page up" })
+    vim.keymap.set("n", "<s-c-d>", function()
+      require("opencode").command("session.half.page.down")
+    end, { desc = "opencode half page down" })
+  end,
+}
